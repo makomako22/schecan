04ae542b919f32ddce94cfe51be8aac0a4aa0364
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2018-04-07 16:04:59 +0900

    edit width

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,3 +6,12 @@ var elem = document.querySelector('.sidenav');
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+var elem = document.querySelector('.modal');
+  var instance = M.Modal.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
