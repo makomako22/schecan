3cdeb83bc12e224efcaf91666d80360513d4fa8f
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2018-04-16 18:42:54 +0900

    add comment

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -12,5 +12,12 @@ class Schedule < ActiveRecord::Base
   validates :schedule_at_time, presence: true
   validates :mentor, presence: true
   belongs_to :student
+
+  def get_term(schedule)
+    student = Student.find_by(term: schedule.student.term)
+    s_term = student.term
+    term = Term.find_by(term_i: s_term)
+    term.term_t
+  end
 end
 
