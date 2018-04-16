11f440a1f8a037c67f01dca9ef0669d40d5f8a9f
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2018-04-16 19:00:55 +0900

    edit student

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,4 +15,10 @@ class Student < ActiveRecord::Base
     "#{family_name_kana} #{first_name_kana}"
   end
 
+  def get_student_term(student)
+    s_term = student.term
+    term = Term.find_by(term_i: s_term)
+    term.term_t
+  end
+
 end
