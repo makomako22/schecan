4eaa56cbf743144e13c4877b5f2030d3c42023e2
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2018-04-21 07:58:58 +0900

    edit student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -49,7 +49,7 @@ class StudentsController < ApplicationController
   def destroy
     student = Student.find(params[:id])
     student.destroy
-    redirect_to students_path, notice: "削除が完了しました"
+    redirect_to basis_path(student.base_id), notice: "削除が完了しました"
   end
 
   def search
